61f4f955ba61948fbed508d34aaf0a7d49ddeae3
quangdangcodegym <quang.dang@codegym.vn>
2023-12-01 14:16:43 +0700

    validate thymeleaf

--- mvcproduct/src/main/java/com/cg/mvcproduct/ProductController.java
+++ mvcproduct/src/main/java/com/cg/mvcproduct/ProductController.java
@@ -1,9 +1,12 @@
 package com.cg.mvcproduct;
 
+import com.cg.mvcproduct.model.Product;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,5 +16,14 @@ public class ProductController {
     public String getAllProducts(Model model) {
         model.addAttribute("message", "HELLO EM YÊU");
         return "list";
+    }
+
+    @GetMapping("/new")
+    public String createProduct(){
+        return "create";
+    }
+    @PostMapping("/new")
+    public String insertProduct(@ModelAttribute("product") Product product, BindingResult result){
+        return "create";
     }
 }
---
+++ mvcproduct/src/main/java/com/cg/mvcproduct/model/Product.java
@@ -0,0 +1,7 @@
+package com.cg.mvcproduct.model;
+
+public class Product {
+    private Long id;
+    private String name;
+    private Float price;
+}
