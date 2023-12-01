455b48ba0a29cbf0f21940898e8fea8699287d3d
dat15061999 <datphamvan49@gmail.com>
2023-12-01 14:40:11 +0700

    add form

--- mvcproduct/src/main/java/com/cg/mvcproduct/model/Product.java
+++ mvcproduct/src/main/java/com/cg/mvcproduct/model/Product.java
@@ -1,7 +1,52 @@
 package com.cg.mvcproduct.model;
 
-public class Product {
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+public class Product implements Validator {
+
     private Long id;
     private String name;
     private Float price;
+
+    public Product(Long id, String name, Float price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return false;
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        String name = errors.getObjectName();
+    }
 }
